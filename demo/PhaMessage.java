6a95eb4128cb98ee9ea0e5f816704f53037d4af5
yunhaibao <yunhaibao@qq.com>
2023-09-12 16:11:00 +0800

    基本完成版本

--- demo/PhaMessage.java
+++ demo/PhaMessage.java
@@ -1,22 +1,43 @@
 public class PhaMessage {
     public static void main(String[] args) {
-        System.out.println(PhaMessage.Msg("U00001","H000001","SUBSCRIPT%IsExistType+3~PHA.SYS.ComDict.Save.1 ~DHCSTSCDI(0,\"TYPE\",\"\")"));
-//        中文唯一弊端就是不通用, 如果不会中文的人翻译中文的字典时, 将完全不能理解也难以区分, 如同我们看韩文日文, 字母+数字,才是最通用且容易识别的
-//        x 因此中文的方式, 不适合
-//        System.out.println(PhaMessage.Msg("代码重复","",""));
-//        
-//        相比中文, 如下英文的方式test较为通用, 也有使用的, 如intersystems 的错误代码
-//        @link http://localhost:57772/csp/docbook/DocBook.UI.Page.cls?KEY=RERR_tsql
-//        System.out.println(PhaMessage.Msg("code is null","",""));
-//
+        String msgInfo = "";
+        msgInfo = PhaMessage.Msg("U00001", "H000001", "SUBSCRIPT%IsExistType+3~PHA.SYS.ComDict.Save.1 ~DHCSTSCDI");
+        System.out.println(msgInfo);
+        // tipinfo / stackInfo 非必要时, 允许为空
+        msgInfo = PhaMessage.Msg("U00002", "", "");
+        System.out.println(msgInfo);
+        // 举例1, 库存不足, tipinfo
+        String[] tipValueArr = {"阿莫西林胶囊"};
+        msgInfo = PhaMessage.Msg("S00001", GetModelTip("M00001", tipValueArr), "");
+        System.out.println(msgInfo);
+        // 举例2, 门诊药房8号窗口还需要取药
+        msgInfo = GetModelTip("M00001", new String[]{"门诊药房", "8号窗口"});
+        System.out.println(msgInfo);
     }
-    public static String Msg(String msgCode, String tipInfo, String strackInfo){
+
+    /**
+     * 公共的返回值处理
+     *
+     * @param msgCode   错误代码
+     * @param tipInfo   提醒信息, 翻译后的内容
+     * @param stackInfo 堆栈信息, 原始信息, 不需要任何处理, 根据情况, 适当截断200个字符等
+     * @return String 拼接的错误信息
+     */
+    public static String Msg(String msgCode, String tipInfo, String stackInfo) {
         String msgContent = "";
         // 测试代码, 此处再调数据库, 获取字典数据, 或者, 再前台匹配? 还是直接前台匹配
         // 基础的错误代码字典在前台应该都加载
         if ("U00001".equals(msgCode)) {
             msgContent = "代码重复";
         }
-        return msgContent + "::" + tipInfo + "::" + msgCode + "::" + strackInfo;
+        if ("S00001".equals(msgCode)) {
+            msgContent = "库存不足";
+        }
+        return msgContent + "::" + tipInfo + "::" + msgCode + "::" + stackInfo;
+    }
+
+    public static String GetModelTip(String tipCode, String[] args) {
+        String tipInfo = "咱也不知道";
+        return String.join(",", args);
     }
 }
